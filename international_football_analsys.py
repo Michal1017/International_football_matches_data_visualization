89e8661c64d7f009225fa4acd6ad2f17ba1dd73a
Michal1017 <michal.stan1017@gmail.com>
2022-10-29 18:16:17 +0200

    Add new database with data about every country

--- international_football_analsys.py
+++ international_football_analsys.py
@@ -1,6 +1,8 @@
+from typing_extensions import Self
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sqlalchemy import null, values
 
 # read data from csv file
 df = pd.read_csv('results.csv')
@@ -25,10 +27,37 @@ print("Number of missing data: ")
 print(df.isnull().sum())
 
 # Extract year, month and day of specific data
-df['year'] = df['date'].str.extract(r'([0-9]{4})', expand=True).astype(int)
-df['month'] = df['date'].str.extract(r'-([0-9]{2})-', expand=True).astype(int)
-df['day'] = df['date'].str.extract(r'-([0-9]{2})$', expand=True).astype(int)
+#df['year'] = df['date'].str.extract(r'([0-9]{4})', expand=True).astype(int)
+df['date'] = pd.DatetimeIndex(df['date'])
 
+df['year'] = df['date'].dt.year
+df['month'] = df['date'].dt.month
+df['day'] = df['date'].dt.day
+
+df['total_goals'] = df['home_score'] + df['away_score']
+
+
+def winning_team(x):
+    if(x.home_score > x.away_score):
+        return x.home_team
+    if(x.home_score < x.away_score):
+        return x.away_team
+    else:
+        return None
+
+
+def defeated_team(x):
+    if(x.home_score > x.away_score):
+        return x.away_team
+    if(x.home_score < x.away_score):
+        return x.home_team
+    else:
+        return None
+
+
+df['winner'] = df.apply(winning_team, axis=1)
+
+df['loser'] = df.apply(defeated_team, axis=1)
 # Check how dataset looks with new features
 print(df.head())
 
@@ -38,45 +67,23 @@ print(df.info())
 # Print statistics of dataset
 print(df.describe())
 
-# Function which help visualize better number of matches through time
-
-
-def get_range_years(x):
-    if(x < 1875):
-        return '1850-1874'
-    if(1875 <= x < 1900):
-        return '1875-1899'
-    if(1900 <= x < 1925):
-        return '1900-1924'
-    if(1925 <= x < 1950):
-        return '1925-1949'
-    if(1950 <= x < 1975):
-        return '1950-1974'
-    if(1975 <= x < 2000):
-        return '1975-1999'
-    if(2000 <= x < 2025):
-        return '2000-2024'
-
-
-df['ranged_years'] = df['year'].apply(get_range_years)
-
-
-# Plot how many matches was played in every year
-sns.barplot(df['ranged_years'].value_counts().index,
-            df['ranged_years'].value_counts().values)
-plt.title('Number of matches in specific year')
+sns.lineplot(x=df['year'].value_counts().index, y=df['year'].value_counts())
+plt.title('Number of played matches through years')
+plt.xlabel('year')
+plt.ylabel('Number of matches')
 plt.show()
 
 # Plot how many games was played in each month
-sns.barplot(df['month'].value_counts().index,
-            df['month'].value_counts().values)
+sns.histplot(df['month'])
 plt.title("Number of matches in each month")
+plt.ylabel('Number of matches')
 plt.show()
 
 # Plot how many games was played in each tourmament
 sns.barplot(df['tournament'].value_counts().index[:10],
             df['tournament'].value_counts().values[:10])
 plt.title('Number of matches in each tourmament')
+plt.ylabel('Number of matches')
 plt.show()
 
 
@@ -96,13 +103,44 @@ print(no_away_game)
 
 # All goals
 print("All goals scored in international football matches: ",
-      df['home_score'].values.sum()+df['home_score'].values.sum())
+      df['total_goals'].values.sum())
 
-
-countries_and_goals = []
 
 print(df.iloc[0]['home_team'])
 
+ser = df['home_team'].value_counts() + df['away_team'].value_counts()
+
+print(ser.index)
+
+countries_df = pd.DataFrame({'Country': ser.index,
+                             'Matches': ser.values.astype(int)})
+
+won_matches_by_country = df['winner'].value_counts()
+lost_matches_by_country = df['loser'].value_counts()
+
+print(won_matches_by_country.sort_values())
 
 
-    
+def matches_result(x, series):
+    if series.index.isin([x]).any():
+        return series[x]
+    else:
+        return 0
+
+
+countries_df['Wins'] = countries_df['Country'].apply(
+    matches_result, series=won_matches_by_country)
+
+countries_df['Losses'] = countries_df['Country'].apply(
+    matches_result, series=lost_matches_by_country)
+
+countries_df['Draws'] = countries_df['Matches'] - \
+    countries_df['Wins'] - countries_df['Losses']
+
+countries_df['Percent of won games'] = countries_df['Wins'] * \
+    100/countries_df['Matches']
+
+
+print(countries_df)
+
+print(df[df['home_team'] == 'England']['home_score'].sum())
